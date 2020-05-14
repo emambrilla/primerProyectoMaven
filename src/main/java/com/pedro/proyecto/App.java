bc8ba2b44f983e32e4747edea720b46adb337a6a
Eduardo Mambrilla <emambrilla@gmail.com>
2020-05-14 17:25:55 +0200

    tercer commit

--- src/main/java/com/pedro/proyecto/App.java
+++ src/main/java/com/pedro/proyecto/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "En un lugar de la mancha, de cuyo nombre no quiero acordarme");
+        System.out.println( " ... " );
     }
 }
